4a8a5837963442132218f595b7c96a9c17e0002d
Alex Bucse <seattlerealestatephoto@gmail.com>
2024-08-27 22:02:04 -0700

    added image viewer for gallery

---
+++ adult-family-home/src/app/component/image-viewer-modal/image-viewer-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-image-viewer-modal',
+  templateUrl: './image-viewer-modal.component.html',
+  styleUrls: ['./image-viewer-modal.component.css']
+})
+export class ImageViewerModalComponent {
+  @Input() imageUrl!: string;
+  @Output() closeModal = new EventEmitter<void>();
+
+  close() {
+    this.closeModal.emit();
+  }
+}
