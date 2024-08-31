6f00e6fb3d497999519165ca8a9911e1c83f13f5
Alex Bucse <seattlerealestatephoto@gmail.com>
2024-08-30 23:19:33 -0700

    added modal confirmation for business created and also toggle on business list to show all / show active businesses

---
+++ adult-family-home/src/app/component/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css']
+})
+export class ConfirmationDialogComponent {
+  @Input() message: string = '';
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+}
