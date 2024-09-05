a6ab7fffcefd2473c83e1afec6d399beec143d62
Monkifoto LLc <monkifoto@gmail.com>
2024-09-04 21:56:13 -0700

    added new section for home page that still need to be updated

---
+++ adult-family-home/src/app/component/testimonials/testimonials.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials',
+  templateUrl: './testimonials.component.html',
+  styleUrls: ['./testimonials.component.css']
+})
+export class TestimonialsComponent {
+  @Input() testimonialText!: string;
+}
