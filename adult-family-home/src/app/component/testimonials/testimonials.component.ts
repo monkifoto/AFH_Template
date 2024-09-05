9184edbe97f7846a994a6b81906af35dc33cd958
Monkifoto LLc <monkifoto@gmail.com>
2024-09-05 14:49:19 -0700

    rem

--- adult-family-home/src/app/component/testimonials/testimonials.component.ts
+++ adult-family-home/src/app/component/testimonials/testimonials.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Testimonial } from 'src/app/model/business-questions.model';
 
 @Component({
   selector: 'app-testimonials',
@@ -6,5 +7,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./testimonials.component.css']
 })
 export class TestimonialsComponent {
-  @Input() testimonialText!: string;
+  @Input() testimonials!: Testimonial[] | undefined;
 }
