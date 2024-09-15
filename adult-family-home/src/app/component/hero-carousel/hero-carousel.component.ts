e2e6c40ba0f3d2372dd60f9b8dd24d56eab92442
Monkifoto LLc <monkifoto@gmail.com>
2024-09-14 21:11:56 -0700

    debugging hero images

--- adult-family-home/src/app/component/hero-carousel/hero-carousel.component.ts
+++ adult-family-home/src/app/component/hero-carousel/hero-carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { from, map, switchMap } from 'rxjs';
 import { Business, HeroImage } from 'src/app/model/business-questions.model';
 import { WebContentService } from 'src/app/services/web-content.service';
 
@@ -32,10 +33,28 @@ export class HeroCarouselComponent implements OnInit {
 
   loadHeroImages(): void {
     const uploadLocation = 'heroImages'; // This is the folder in Firestore where the images are stored
-    this.webContentService.getBusinessUploadedImagesById(this.businessId, uploadLocation)
-      .subscribe(images => {
+    this.webContentService.getBusinessUploadedImagesById(this.businessId, uploadLocation).pipe(
+      switchMap(images => {
+
+        // Create an array of observables that check if the image exists
+        const checks = images.map(async image => {
+          const exists = await this.webContentService.checkImageExists(image.url);
+          return exists ? image : null;  // Return image only if it exists
+        });
+
+        return from(Promise.all(checks));
+      }),
+      map(images =>images.filter(image => image !== null))
+    ).subscribe(images => {
         this.heroImages = images; // Store the retrieved images
+        console.log("Number of images: ",this.heroImages.length);
+
+        this.heroImages.forEach(element => {
+          console.log(element.altText);
+
+        });
       });
+
   }
 
 navigateToContact() {
