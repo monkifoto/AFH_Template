539dde50e4bab8cbd8db0aac2f050bb0672b0958
Alex Bucse <seattlerealestatephoto@gmail.com>
2024-10-23 23:14:05 -0700

    moved reviews and services and contact us to individual components sort of

--- adult-family-home/src/app/admin/contact-us-page/contact-us-page.component.ts
+++ adult-family-home/src/app/admin/contact-us-page/contact-us-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us-page',
   templateUrl: './contact-us-page.component.html',
   styleUrls: ['./contact-us-page.component.css']
 })
-export class ContactUsPageComponent {
+export class ContactUsPageComponent implements OnInit {
+  @Input() form!: FormGroup;
+
+  predefinedImages = [
+    { url: 'assets/sharedAssets/image_fx_(1).jpg' },
+    { url: 'assets/sharedAssets/image_fx_(2).jpg' },
+    { url: 'assets/sharedAssets/image_fx_(3).jpg' },
+    { url: 'assets/sharedAssets/image_fx_(4).jpg' },
+    { url: 'assets/sharedAssets/image_fx_(5).jpg' },
+  ];
+
+
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // Initialize the form with necessary controls
+    this.form = this.fb.group({
+      selectedImageUrl: [''],  // Control for image selection
+      contactFormDetails: [''],
+      mapIframeUrl: ['']
+    });
+  }
+
+  onImageSelection(url: string) {
+    this.form.patchValue({
+      selectedImageUrl: url
+    });
+  }
 
 }
--- adult-family-home/src/app/admin/reviews/reviews.component.ts
+++ adult-family-home/src/app/admin/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { finalize, Observable } from 'rxjs';
+import { UploadService } from 'src/app/services/upload.service';
 
 @Component({
   selector: 'app-reviews',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviews.component.css']
 })
 export class ReviewsComponent {
+  @Input() form!: FormGroup;
+  businessId!: ""; //to do: need this value for uploading images
+  uploadProgress: { [key: string]: Observable<number | undefined> } = {};
 
+  constructor(private fb: FormBuilder,
+    private uploadService: UploadService,
+  ) {}
+
+
+
+  get testimonials(): FormArray {
+    return this.form.get('testimonials') as FormArray;
+  }
+
+  addTestimonial(): void {
+    const testimonialForm = this.fb.group({
+      id: [''],
+      name: [''],
+      quote: [''],
+      photoURL: ['']
+    });
+    this.testimonials.push(testimonialForm);
+  }
+
+  removeTestimonial(index: number): void {
+    this.testimonials.removeAt(index);
+  }
+
+  onTestimonialFileChange(event: any, index: number): void {
+    const file = event.target.files[0];
+    if (file) {
+      const { uploadProgress, downloadUrl } = this.uploadService.uploadFile(file, this.businessId, 'testimonail');
+
+      this.uploadProgress[`testimonails_${index}`] = uploadProgress;
+
+      downloadUrl.pipe(
+        finalize(() => {
+          downloadUrl.subscribe(url => {
+            this.testimonials.at(index).patchValue({ photoURL: url });
+          });
+        })
+      ).subscribe();
+    }
+  }
 }
