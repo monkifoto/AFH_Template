06df0b641db0b8bd3a982fc5a2943026529c0050
Monkifoto LLc <monkifoto@gmail.com>
2024-11-06 23:38:43 -0800

    changes to styles

--- adult-family-home/src/app/app-routing.module.ts
+++ adult-family-home/src/app/app-routing.module.ts
@@ -11,25 +11,11 @@ import { LoginComponent } from './admin/login/login.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  //{ path: ':id', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'gallery', component: PhotoGalleryComponent },
   { path: 'login', component: LoginComponent },
-
-  // { path: 'gallery', component: PhotoGalleryComponent},
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // { path: '**', redirectTo: '/home' },
-  // { path: 'bqs', component: BusinessQuestionFormComponent },
-  // { path: 'bls', component: BusinessListComponent},
-  // { path: 'business/new', component: BusinessQuestionFormComponent },
-  // { path: 'business/edit/:id', component: EditBusinessComponent },
-
-  // { path: 'businessList', component: BusinessListComponent },
-  // { path: 'new-business', component: BusinessQuestionFormComponent },
-  // { path: 'edit-business/:id', component: EditBusinessComponent },
-  // { path: 'gallery-upload', component: PhotoGalleryUploadComponent},
 
 
     // Admin routes (must come before the dynamic :id route to avoid conflicts)
--- adult-family-home/src/app/app.component.ts
+++ adult-family-home/src/app/app.component.ts
@@ -29,7 +29,6 @@ export class AppComponent implements OnInit {
       this.businessId = params['id'];
       // Load the business data and theme after fetching the business ID
       this.loadBusinessData();
-      //this.loadThemeColors();
     });
   }
 
@@ -41,61 +40,6 @@ export class AppComponent implements OnInit {
       }
     });
   }
-
-  // Fetch theme colors and apply them
-  // loadThemeColors() {
-  //   this.themeService.getThemeColors(this.businessId).subscribe((themeColors) => {
-  //     console.log("app.component:  Theme colors: ", themeColors);
-  //     if (themeColors && this.hasValidColors(themeColors)) {
-  //       this.colors = themeColors;
-
-  //       this.applyTheme();
-  //     } else {
-  //       console.warn('No valid colors found in the database. Applying default theme.');
-  //       // Fallback to default theme if no valid colors found
-  //       this.themeService.resetToDefaultColors().subscribe((defaultColors) => {
-  //         if (defaultColors && this.hasValidColors(defaultColors)) {
-  //           this.colors = defaultColors;
-  //         } else {
-  //           // Use hardcoded defaults if all fails
-  //           this.colors = {
-  //             primaryColor: '#fffaf2',
-  //             secondaryColor: '#f8f3f0',
-  //             accentColor: '#F0C987',
-  //             backgroundColor: '#F5F3E7',
-  //             darkBackgroundColor: '#4C6A56',
-  //             textColor: '#2F2F2F',
-  //             navBackgroundColor: '#F5F3E7',
-  //             navTextColor: '#33372C',
-  //             navActiveBackground: '#33372C',
-  //             navActiveText: '#ffffff',
-  //             buttonColor: '#D9A064',
-  //             buttonHoverColor: '#c9605b'
-  //           };
-  //         }
-  //         this.applyTheme();
-  //       });
-  //     }
-  //   }, (error) => {
-  //     console.error('Error fetching theme colors: ', error);
-  //     // Apply default theme on error
-  //     this.colors = {
-  //       primaryColor: '#fffaf2',
-  //       secondaryColor: '#f8f3f0',
-  //       accentColor: '#F0C987',
-  //       backgroundColor: '#F5F3E7',
-  //       darkBackgroundColor: '#4C6A56',
-  //       textColor: '#2F2F2F',
-  //       navBackgroundColor: '#F5F3E7',
-  //       navTextColor: '#33372C',
-  //       navActiveBackground: '#33372C',
-  //       navActiveText: '#ffffff',
-  //       buttonColor: '#D9A064',
-  //       buttonHoverColor: '#c9605b'
-  //     };
-  //     this.applyTheme();
-  //   });
-  // }
 
 
 
