07070f89e7ac5b4dc408276b76842501272d1ded
Alex Bucse <seattlerealestatephoto@gmail.com>
2024-11-08 00:30:55 -0800

    new services section for AE

--- adult-family-home/src/app/component/our-services-home/our-services-home.component.ts
+++ adult-family-home/src/app/component/our-services-home/our-services-home.component.ts
@@ -10,4 +10,10 @@ import { Component, Input } from '@angular/core';
 export class OurServicesHomeComponent {
   @Input() services!: { icon: string, title: string, description: string }[];
   @Input() layoutType: string = 'demo';
+  expandedServiceIndex: number | null = null;
+
+  toggleService(index: number) {
+    // Toggle the service description on or off
+    this.expandedServiceIndex = this.expandedServiceIndex === index ? null : index;
+  }
 }
