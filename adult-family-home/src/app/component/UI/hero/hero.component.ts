0e6b2696ec7f0fa69fb023c51188016043aaed47
Monkifoto LLc <monkifoto@gmail.com>
2024-11-12 11:09:19 -0800

    hero fix and added UI testimonial

--- adult-family-home/src/app/component/UI/hero/hero.component.ts
+++ adult-family-home/src/app/component/UI/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
@@ -11,17 +11,20 @@ export class HeroComponent implements  OnInit {
   message: string = '';
 
   private imagesAndMessages: { [key in 'services' | 'gallery' | 'about-us' | 'contact-us']: { image: string; message: string } } = {
-    'services': { image: '../../../../assets/sharedAssets/istockphoto-1207318385-2048x2048.jpg', message: 'Discover our Services' },
+    'services': { image: '../../../../assets/sharedAssets/istockphoto-1344063915-2048x2048.jpg', message: 'Discover our Services' },
     'gallery': { image: '../../../../assets/sharedAssets/istockphoto-1551967154-2048x2048.jpg', message: 'Explore our Gallery' },
-    'about-us': { image: '../../../../assets/sharedAssets/istockphoto-804432288-2048x2048.jpg', message: 'Learn About Us' },
+    'about-us': { image: '../../../../assets/sharedAssets/istockphoto-1162510523-2048x2048.jpg', message: 'Learn About Us' },
     'contact-us': { image: '../../../../assets/sharedAssets/istockphoto-1066099806-2048x2048.jpg', message: 'Get in Touch' },
   };
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const page = params.get('page') as 'services' | 'gallery' | 'about-us' | 'contact-us'; // Get 'page' from the route parameter
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url.split('?')[0]; // Get the path without query params
+      const page = currentUrl.replace('/', '') as 'services' | 'gallery' | 'about-us' | 'contact-us';
+      console.log('UI - Hero, page: ', page);
+
 
       if (page && this.imagesAndMessages[page]) {
         this.backgroundImage = `url(${this.imagesAndMessages[page].image})`;
