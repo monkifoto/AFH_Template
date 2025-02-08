790d08306dadc72443103a3207af9ff768d2314c
Alex Bucse <seattlerealestatephoto@gmail.com>
2025-02-08 15:53:23 -0800

    added description for packages for SB

--- adult-family-home/src/app/component/UI/latest-products/latest-products.component.ts
+++ adult-family-home/src/app/component/UI/latest-products/latest-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-latest-products',
@@ -39,7 +41,7 @@ export class LatestProductsComponent implements OnInit {
     title: 'LaunchPad Website',
     companyLogo: '',
     category: '$799',
-    description: 'Get your adult family home online quickly with a simple yet professional single-page website. Perfect for a clean, mobile-friendly presence that showcases your services, location, and contact details. This package is great for those who want an affordable, no-fuss solution to establish credibility online.',
+    description: 'Get your adult family home online quickly with a simple yet professional single-page website. Perfect for a clean, mobile-friendly presence that showcases your services, location, and contact details. This package is great for those who want an affordable, no-fuss solution to establish credibility online.<br/><br/><ul><li>2 Years Hosting</li><li>Owner-Provided Photos</li><li>Onwer-Provided Logo</li><li>Custom Color Theme</li><li>1 year Maintenance & Updates</li></ul>',
     image: 'https://firebasestorage.googleapis.com/v0/b/afhdynamicwebsite.appspot.com/o/businesses%2FMGou3rzTVIbP77OLmZa7%2FheroImages%2FA7400076.jpg?alt=media&token=1132bd32-3d2c-4f52-be31-3759c20b5285',
      url: ''
   },
@@ -47,7 +49,7 @@ export class LatestProductsComponent implements OnInit {
     title: 'Foundation Website',
     companyLogo: '',
     category: '$1500',
-    description: 'A well-rounded five-page website designed to highlight all the key aspects of your adult family home. This package includes essential pages such as Home, About, Services, Gallery, and Contact, ensuring visitors get a complete picture of your offerings. SEO-optimized, mobile-responsive, and designed to attract potential residents and their families.',
+    description: 'A well-rounded five-page website designed to highlight all the key aspects of your adult family home. This package includes essential pages such as Home, About, Services, Gallery, and Contact, ensuring visitors get a complete picture of your offerings. SEO-optimized, mobile-responsive, and designed to attract potential residents and their families.<br/><br/><ul><li>3 Years of Secure Hosting</li><li>Proffessional Video</li><li>High-Quality Professional Photography</li><li>Custom Logo Design</li><li>Tailored Website Design</li><li>2 Years of Maintenance & Updates</li></ul>',
     image: 'https://firebasestorage.googleapis.com/v0/b/afhdynamicwebsite.appspot.com/o/businesses%2FMGou3rzTVIbP77OLmZa7%2FheroImages%2FDJI_0061.jpg?alt=media&token=24aa3da1-a50d-47ec-89ae-588eb13e42a9',
      url: ''
   },
@@ -55,12 +57,19 @@ export class LatestProductsComponent implements OnInit {
     title: 'Prestige Website',
     companyLogo: '',
     category: 'Pricing varies',
-    description: 'A high-end, fully customized website tailored to your brand. This package includes professional photography and videography, custom design, logo creation, business card design, and advanced features such as integrated Google Maps, resident intake forms, and review sections. Perfect for those who want to stand out with a premium online presence.',
+    description: 'A high-end, fully customized website tailored to your brand. This package includes professional photography and videography, custom design, logo creation, business card design, and advanced features such as integrated Google Maps, resident intake forms, and review sections. Perfect for those who want to stand out with a premium online presence. <br/><br/><ul><li>5 Years of Secure Hosting</li><li>Cinematic Professional Video</li><li>High-Resolution Professional Photography</li><li>Immersive Virtual Tour</li><li>CBespoke Logo Design</li><li>Exclusive, Custom Website Design</li><li>Comprehensive Maintenance & Updates</li></ul>',
     image: 'https://firebasestorage.googleapis.com/v0/b/afhdynamicwebsite.appspot.com/o/businesses%2FMGou3rzTVIbP77OLmZa7%2FheroImages%2FDJI_0713%20DTE-Edit.jpg?alt=media&token=31151dd0-c6fb-4cc1-acf8-eb50d617d318',
      url: ''
   }];
 
   displayItems: any[] = [];
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  getSanitizedDescription(description: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(description);
+  }
+
 
   ngOnInit(): void {
     this.displayItems = this.layoutType === 'sb' ? this.products : this.companies;
