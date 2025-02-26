6e77592c25ccbb367abdf194dcb7ed387474b993
Alex Bucse <seattlerealestatephoto@gmail.com>
2025-02-26 00:05:02 -0800

    small fixes

--- adult-family-home/src/app/component/UI/item-list/item-list.component.ts
+++ adult-family-home/src/app/component/UI/item-list/item-list.component.ts
@@ -6,19 +6,21 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
-  @Input() sectionTitle: string = 'Our Services';  // ✅ Dynamically set from the section
+  @Input() title: string = 'Our Services';  // ✅ Dynamically set from the section
+  @Input() subTitle:string ='';
   @Input() items: any[] = []; // ✅ List of items in the section
   @Input() isMinimal: boolean = false; // ✅ Controls minimal/full view
   @Input() showLearnMore: boolean = false; // ✅ Toggle Learn More button
   @Input() sectionImageUrl: string | null = null; // ✅ Background image
   @Input() isParallax: boolean = true; // ✅ Controls whether parallax effect is applied
-  @Input() layoutType: string = 'demo';
+  @Input() themeType: string = 'demo';
 
   constructor() {}
 
   ngOnInit(): void {
     console.log("📌 Item List Initialized:", {
-      title: this.sectionTitle,
+      title: this.title,
+      subTitle: this.subTitle,
       items: this.items,
       isMinimal: this.isMinimal,
       sectionImageUrl: this.sectionImageUrl,
